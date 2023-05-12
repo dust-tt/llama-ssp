91413136e5efff8e8790d5d6d60370ebc19fa160
philipperolet <philipperolet@gmail.com>
2023-05-12 10:55:22 +0200

    added batch of 16, averaging speeds on 16 contexts

--- llamassp.py
+++ llamassp.py
@@ -5,12 +5,29 @@ from transformers import LlamaTokenizer, LlamaForCausalLM
 
 from lssp.ssp import ssp, FakeModel
 
-MAX_NEW_TOKENS = 64
+MAX_NEW_TOKENS = 32
 llama7b_name = 'decapoda-research/llama-7b-hf'
 llama13b_name = 'decapoda-research/llama-13b-hf'
 batch_size = 1
 
-text = 'In which country is Hamburg?\n'
+texts = [
+    'In which country is Hamburg?\n',
+    'How are you doing today?\n',
+    'It was a dark and stormy night.',
+    'The sun rose slowly over the horizon, casting a warm glow on the world below.',
+    'I never believed in ghosts until the day I met one.',
+    'The sound of the train whistle echoed through the valley as I stood at the station, waiting.',
+    'She walked into the room and everything changed.',
+    'The smell of freshly baked bread filled the air as I entered the bakery.',
+    'The first time I saw her, I knew she was trouble.'
+    'The world was ending, and I was the only one who knew.',
+    'It was the best of times, it was the worst of times.',
+    'The forest was alive with the sound of animals as I walked deeper into the woods.',
+    'As I looked out over the city, I knew that anything was possible.',
+    'The sound of gunfire echoed through the streets as I ran for cover.',
+    'The waves crashed against the shore, a never-ending cycle of destruction and creation.',
+    'I woke up to find myself in a strange place, with no memory of how I got there.',
+    'The clock struck midnight, and I knew that my life would never be the same.',]
 tokenizer = LlamaTokenizer.from_pretrained(llama7b_name)
 
 # free_in_GB = int(torch.cuda.mem_get_info()[0]/1024**3)
@@ -23,7 +40,7 @@ tokenizer = LlamaTokenizer.from_pretrained(llama7b_name)
 def create_model(model_name, max_memory, load_in_8bit=True):
     return LlamaForCausalLM.from_pretrained(
         model_name,
-        device_map='auto',
+        device_map='sequential',
         load_in_8bit=load_in_8bit,
         max_memory=max_memory
     )
@@ -32,10 +49,17 @@ def create_model(model_name, max_memory, load_in_8bit=True):
 def time_model(model):
     # time the first run
     start_time = time.time()
-    input_ids = tokenizer(text, return_tensors="pt").input_ids
-    input_ids = torch.stack([input_ids[0]] * batch_size).to(model.device)
-    generated_ids = model.generate(input_ids, max_length=MAX_NEW_TOKENS)
-    nb_tokens = generated_ids.shape[1] - input_ids.shape[1]
+    nb_tokens = 0
+    for text in texts:
+        print("Completing text:", text)
+        intermediate_time = time.time()
+        input_ids = tokenizer(text, return_tensors="pt").input_ids
+        input_ids = torch.stack([input_ids[0]] * batch_size).to(model.device)
+        generated_ids = model.generate(input_ids, max_length=MAX_NEW_TOKENS)
+        nb_tokens += generated_ids.shape[1] - input_ids.shape[1]
+        print("Completion: ", tokenizer.decode(generated_ids[0], skip_special_tokens=True))
+        print("Time: {:.2f}s".format(time.time() - intermediate_time))
+        print("========\n")
     token_per_sec = nb_tokens / (time.time() - start_time)
     return generated_ids, token_per_sec
 
@@ -53,7 +77,7 @@ models_params = {
                 'max_memory': {0: '10GB'},
                 'load_in_8bit': True},
     '13B_8bit': {'model_name': llama13b_name,
-                 'max_memory': {0: '19GB'},
+                 'max_memory': {0: '20GB'},
                  'load_in_8bit': True},
 }
 
@@ -63,7 +87,7 @@ def print_speeds(speeds):
     for model_name, tokens_s in speeds.items():
         print('-'*20)
         print(f"{model_name} |  {tokens_s:.2f}/s")
-        print('-'*20)
+    print('-'*20)
 
 
 if __name__ == '__main__':
